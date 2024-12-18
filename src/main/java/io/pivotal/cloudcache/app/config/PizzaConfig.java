06b9e8b3b992692df5f3348154a451ace1711e35
Amit Shrivastav <amit.shrivastav@broadcom.com>
2024-12-18 13:52:38 +0530

    Revert "Update pizza app dependencies latest versions of Springboot for gemfire and Vmware Gemfire."
    This reverts commit 9c8cd23499fe613153f83cedc778c4c27e18089f.

--- src/main/java/io/pivotal/cloudcache/app/config/PizzaConfig.java
+++ src/main/java/io/pivotal/cloudcache/app/config/PizzaConfig.java
@@ -2,30 +2,32 @@ package io.pivotal.cloudcache.app.config;
 
 import org.apache.geode.cache.client.SocketFactory;
 import org.apache.geode.cache.client.proxy.SniProxySocketFactory;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.gemfire.config.annotation.EnableClusterConfiguration;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 import org.springframework.data.gemfire.config.support.RestTemplateConfigurer;
-import org.springframework.geode.config.annotation.EnableClusterAware;
 import org.springframework.geode.config.annotation.EnableDurableClient;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.ResourceUtils;
 
 import javax.net.ssl.SSLContext;
 import java.io.File;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 @Configuration
 @EnableDurableClient(id = "pizza-store")
 @EnableEntityDefinedRegions(basePackages = "io.pivotal.cloudcache.app.model")
-@EnableClusterAware
+@EnableClusterConfiguration(useHttp = true)
 public class PizzaConfig {
 
     @Profile({"off-platform", "app-foundation"})
@@ -54,16 +56,8 @@ public class PizzaConfig {
                 .loadTrustMaterial(new File(truststorePath), trustStorePassword.toCharArray())
                 .build();
 
-        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(
-                        SSLConnectionSocketFactoryBuilder.create()
-                                .setSslContext(sslContext)
-                                .build()
-                )
-                .build();
-        CloseableHttpClient client = HttpClients
-                .custom()
-                .setConnectionManager(connectionManager)
+        HttpClient client = HttpClients.custom()
+                .setSslcontext(sslContext)
                 .build();
 
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
